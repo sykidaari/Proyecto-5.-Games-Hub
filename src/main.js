bb99a5cc50acbd482395b83ac4ab290e3bc7b48e
margar3tas <k.mikae.lieb@gmail.com>
2025-04-28 01:03:13 +0100

    finished

--- src/main.js
+++ src/main.js
@@ -2,25 +2,3 @@ import './main.scss';
 import { home } from './pages/home';
 
 home();
-
-// TICTACTOE
-
-// DONE :)
-
-// MEMORY
-// DONE :)
-
-// GAME 3
-// DONE
-
-// SWITCH BETWEEN GAMES
-//  DONE, CHECK
-
-//? GEN PAGE STYLES + responsive
-
-//?HTML HEAD
-
-//? USE HTML CHECKER TO CHECK HTML
-
-//  CLEAN CODE
-//  DONE :))
